f93d6caaffe9c2653306b5651b3cb675d3c0801f
xuxiaojie@youzan.com <xuxiaojie@youzan.com>
2018-09-25 10:35:18 +0800

    测试rx

--- account/src/main/java/com/github/pgycode/account/LoginActivity.kt
+++ account/src/main/java/com/github/pgycode/account/LoginActivity.kt
@@ -2,14 +2,50 @@ package com.github.pgycode.account
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.Scheduler
+import io.reactivex.disposables.Disposable
+import kotlinx.android.synthetic.main.login_activity.*
+
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var observable: Observable<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
+
+        observable = Observable.create {
+            emitter ->
+            emitter.onNext("123")
+            emitter.onNext("1234")
+            emitter.onNext("12345")
+        }
+
+        observable.subscribe(object: Observer<String>{
+
+            override fun onComplete() {
+                println("onComplete")
+            }
+
+            override fun onNext(t: String) {
+                println(t)
+            }
+
+            override fun onError(e: Throwable) {
+                println(e.message)
+            }
+
+            override fun onSubscribe(d: Disposable) {
+                println("开始了")
+            }
+
+        })
+
     }
 
-    
-
 }
+
+
---
+++ account/src/main/java/com/github/pgycode/account/WelcomeActivity.kt
@@ -0,0 +1,15 @@
+package com.github.pgycode.account
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.welcome_activity)
+
+        startActivity(Intent(this, LoginActivity::class.java))
+    }
+}
